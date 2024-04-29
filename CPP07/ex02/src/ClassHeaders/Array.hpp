4c9b0fc30a0e75453c778db5c01be569c02fc6ae
Victor Denissen <vdenisse@student.s19.be>
2024-04-29 10:16:37 +0200

    Finish ex02
    removed .cpp file, all is now in hpp
    bracket overloading

--- CPP07/ex02/src/ClassHeaders/Array.hpp
+++ CPP07/ex02/src/ClassHeaders/Array.hpp
@@ -1,32 +1,101 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+#include <cstddef>
+#include <exception>
+#include <iostream>
+#include <ostream>
+
 // Custom class: Array
-template<class T>
+template<class T = int>
 class	Array
 {
 	public:
 		// Constructor
-		Array	(	void	);
-		Array	(	T	);
+		Array<T>	(	void	)
+		{
+			_n = 0;
+			_var = new T[_n];
+		};
+		Array<T>	(	size_t n	)
+		{
+			_n = n;
+			_var = new T[n];
+		}
+		Array<T>	(	T, size_t n	)
+		{
+			_n = n;
+			_var = new T[n];
+		}
 
 		// Copy constructor
-		Array	(	const Array& other	);
+		Array<T>	(	const Array<T>& other	)
+		{
+			*this = other;
+		};
 
 		// Copy assignment operator overload
-		Array& operator = (const Array& other);
+		Array& operator = (const Array& other)
+		{
+			/* std::cout << "Array:\tcopying object (assignment)\n"; */
+			if (this != &other)
+			{
+				_n = other.getSize();
+				_var = new T[_n];
+				for (size_t n = 0; n < _n; ++n)
+					_var[n] = other[n];
+			}
+			return (*this);
+		};
+
+		// Straight bracket operator overload
+		T& operator [] (size_t n)const
+		{
+			if (n >= _n)
+				throw outOfBoundsException();
+			return _var[n];
+
+		}
+
+		// Out of bounds exception
+		class outOfBoundsException: public std::exception
+		{
+			public :
+			virtual char * what(void)const throw(){
+				return ((char *)"index out of bounds\n");
+			}
+
+		};
+
 
 		// Destructor
-		~Array<T>	(void);
+		~Array<T>	(void)
+		{
+			delete _var;
+		};
 
-		// Getters and Setter
+		void	setNElement	(	T t, size_t n	)
+		{
+			if ( n >= _n)
+				throw outOfBoundsException();
+			_var[n] = t;
+		};
+
+		T *	getVar(	void	)const{return _var;};
+		size_t	getSize	(	void	)const{return _n;}
 
 	protected:
-		/*_*/;
 
 	private:
-		T _var;
-		/*_*/;
+		T *	_var;
+		size_t	_n;
 };
 
+template <typename T>
+std::ostream& operator << (std::ostream& o, Array<T> arr)
+{
+	for (size_t n = 0 ; n < arr.getSize(); n++)
+		o << arr[n] << " ";
+	return o;
+}
 #endif
--- CPP07/ex02/src/main.cpp
+++ CPP07/ex02/src/main.cpp
@@ -1,5 +1,33 @@
 #include "ClassHeaders/Array.hpp"
-int main(void)
+#include <iostream>
+#define SIZE 10
+
+int main( void )
 {
-	Array<char> ar;
+	std::cout << "-----------------------------------------------------" << std::endl;
+
+	Array<  > test;
+
+	Array< int > intArray( SIZE );
+	Array< int > intArray2( SIZE - 5 );
+
+	for ( unsigned int i = 0; i < intArray.getSize(); i++ )
+		intArray[i] = i * 2;
+
+	std::cout << "Int Array 1: " << intArray << std::endl;
+
+	intArray2 = intArray;
+
+	std::cout << "int Array 2: " << intArray2 << std::endl;
+
+	try {
+		std::cout << "Accessing a valid index: " << intArray[5] << std::endl;
+		std::cout << "Accessing an invalid index: " << intArray[SIZE] << std::endl;
+	} catch ( Array< int >::outOfBoundsException& e ) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+
+	std::cout << "-----------------------------------------------------" << std::endl;
+
+	return ( 0 );
 }
