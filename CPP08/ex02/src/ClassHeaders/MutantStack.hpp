195abe02445808f3ffc470be2cb58c50b7b463b0
Victor Denissen <vdenisse@student.s19.be>
2024-05-27 14:05:36 +0200

    cpp08 ex02
    made

--- CPP08/ex02/src/ClassHeaders/MutantStack.hpp
+++ CPP08/ex02/src/ClassHeaders/MutantStack.hpp
@@ -2,27 +2,43 @@
 # define MUTANTSTACK_HPP
 
 // Custom class: MutantStack
+#include <deque>
 #include <stack>
-template<class T>
-class	MutantStack : public std::stack<T>
+template<class T, class Container = std::deque< T > >
+class	MutantStack : public std::stack<T, Container>
 {
 	public:
 		// Constructor
 		MutantStack	(	void	){};
 
 		// Copy constructor
-		MutantStack	(	const MutantStack& other	);
+		MutantStack	(	const MutantStack& that	)
+		{
+			*this = that;	
+		}
 
 		// Copy assignment operator overload
-		MutantStack& operator = (const MutantStack& other);
+		MutantStack& operator = (const MutantStack& that)
+		{
+			std::stack<T, Container>::operator=(that);
+			return *this;
+		}
 
 		// Destructor
 		~MutantStack	(void){};
 
-		struct iterator
-		{
+		typedef typename Container::iterator	iterator;
 
-		};
+		iterator	begin()
+		{
+			return this->c.begin();
+		}
+		iterator	end()
+		{
+			return this->c.end();
+		}
+
+	
 	protected:
 		/*_*/;
 
--- CPP08/ex02/src/main.cpp
+++ CPP08/ex02/src/main.cpp
@@ -1,17 +1,103 @@
 #include "ClassHeaders/MutantStack.hpp"
 #include <iostream>
+#include <list>
 int main(void)
 {
-	std::stack<int> stack;
+	{
+		MutantStack<int> m;
+		m.push(1);
+		m.push(2);
+		m.push(123);
+		m.push(987);
+		m.push(456);
+		m.push(0);
 
-	stack.push(1);
-	stack.push(2);
-	stack.push(3);
-	std::cout << stack.top() << std::endl;
-	stack.pop();
-	std::cout << stack.top() << std::endl;
+		MutantStack<int>::iterator i = m.begin();
+		MutantStack<int>::iterator ie = m.end();
 
-	MutantStack<int> m;
-	m.push(1);
+		while (i != ie)
+		{
+			std::cout << *i << std::endl;
+			i++;
+		}
 
+		MutantStack<int> copy(m);
+		i = copy.begin();
+		ie = copy.end();
+		while (i != ie)
+		{
+			std::cout << *i << std::endl;
+			i++;
+		}
+
+		MutantStack<int> assign = m;
+		i = assign.begin();
+		ie = assign.end();
+		while (i != ie)
+		{
+			std::cout << *i << std::endl;
+			i++;
+		}
+	}
+	{
+		std::list<int> m;
+		m.push_back(1);
+		m.push_back(2);
+		m.push_back(123);
+		m.push_back(987);
+		m.push_back(456);
+		m.push_back(0);
+		std::list<int>::iterator i = m.begin();
+		std::list<int>::iterator ie = m.end();
+
+		while (i != ie)
+		{
+			std::cout << *i << std::endl;
+			i++;
+		}
+
+		std::list<int> copy(m);
+		i = copy.begin();
+		ie = copy.end();
+		while (i != ie)
+		{
+			std::cout << *i << std::endl;
+			i++;
+		}
+
+		std::list<int> assign = m;
+		i = assign.begin();
+		ie = assign.end();
+		while (i != ie)
+		{
+			std::cout << *i << std::endl;
+			i++;
+		}
+	}
 }
+
+/* int main() */
+/* { */
+/* 	MutantStack<int> mstack; */
+/* 	mstack.push(5); */
+/* 	mstack.push(17); */
+/* 	std::cout << mstack.top() << std::endl; */
+/* 	mstack.pop(); */
+/* 	std::cout << mstack.size() << std::endl; */
+/* 	mstack.push(3); */
+/* 	mstack.push(5); */
+/* 	mstack.push(737); */
+/* 	//[...] */
+/* 	mstack.push(0); */
+/* 	MutantStack<int>::iterator it = mstack.begin(); */
+/* 	MutantStack<int>::iterator ite = mstack.end(); */
+/* 	++it; */
+/* 	--it; */
+/* 	while (it != ite) */
+/* 	{ */
+/* 		std::cout << *it << std::endl; */
+/* 		++it; */
+/* 	} */
+/* 	std::stack<int> s(mstack); */
+/* 	return 0; */
+/* } */
